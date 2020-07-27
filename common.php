4e436946eec61331fab882fc654d107ad065a0e6
adwivedi008 <67369950+adwivedi008@users.noreply.github.com>
2020-07-27 19:05:59 +0530

    Add files via upload

---
+++ common.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Interstellar Association</title>
+        <link rel="stylesheet" href="./css/logo.css">
+        <link rel="stylesheet" href="./css/nav.css">
+        <link rel="stylesheet" href="./css/slider1.css">
+        <link rel="stylesheet" href="./css/section1.css">
+        <link rel="stylesheet" href="./css/section2.css">
+        <link rel="stylesheet" href="./css/slider2.css">
+        <link rel="stylesheet" href="./css/footer.css">
+    </head>
+    <body>
+       <!--Header-->
+        <header>
+            <!--Logo-->
+            <div class="box">
+               <div class="top_head">
+                   <img class="logo" src="./images/Logo.png" alt="Interstellar Logo">
+                   <h2 id="cname">Interstellar Association</h2>
+               </div>
+            </div>
+            <br>
+            <!--Navigation Bar-->
+            <div class="box">
+                <div class="top-nav">
+                    <nav>
+                        <ul>
+                            <li>
+                                Home
+                            </li>
+                            <li>
+                                Webinar
+                                <ul class="sub-nav">
+                                    <li>Enquiry</li>
+                                    <li>
+                                        Registration
+                                        <ul class="sub-nav">
+                                            <li>free</li>
+                                            <li>Paid</li>
+                                       </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                Work with us
+                                 <ul class="sub-nav">
+                                     <li>Intern Application</li>
+                                </ul>
+                            </li>
+                            <li>
+                                Queries
+                            </li>
+                            <li>
+                                Blog
+                            </li>
+                            <li>
+                                Support
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <br>
+        </header>
+       <br>
+       <div>
+           
+       </div>
+       <br>
+       <!--footer-->
+        <footer>
+           <div class="box">
+                   <div class="foot" id="left">
+                          <p>&copy; COPYRIGHTS RESERVED BY INTERSTELLAR AND TEAM.</p>
+                      </div>
+                   <div class="foot" id="right">
+                        <a href="https://forms.gle/FmELFTCm9DjiYASFA" title="Developer Contact">info</a>
+                      </div>
+               </div>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Interstellar</title>
+        <link rel="stylesheet" href="./css/logo.css">
+        <link rel="stylesheet" href="./css/nav.css">
+        <link rel="stylesheet" href="./css/slider1.css">
+        <link rel="stylesheet" href="./css/section1.css">
+        <link rel="stylesheet" href="./css/section2.css">
+        <link rel="stylesheet" href="./css/slider2.css">
+        <link rel="stylesheet" href="./css/footer.css">
+    </head>
+    <body>
+        <!--Header-->
+        <header>
+            <!--Logo-->
+            <div class="box">
+               <div class="top_head">
+                   <img class="logo" src="./images/Logo.png" alt="Interstellar Logo">
+                   <h2 id="cname">Interstellar Association</h2>
+               </div>
+            </div>
+            <br>
+            <!--Navigation Bar-->
+            <div class="box">
+                <div class="top-nav">
+                    <nav>
+                        <ul>
+                            <li>
+                                Home
+                            </li>
+                            <li>
+                                Webinar
+                                <ul class="sub-nav">
+                                    <li>Enquiry</li>
+                                    <li>
+                                        Registration
+                                        <ul class="sub-nav">
+                                            <li>free</li>
+                                            <li>Paid</li>
+                                       </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                Work with us
+                                 <ul class="sub-nav">
+                                     <li>Intern Application</li>
+                                </ul>
+                            </li>
+                            <li>
+                                Queries
+                            </li>
+                            <li>
+                                Blog
+                            </li>
+                            <li>
+                                Support
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <br>
+        </header>
+        <!--Slider_1-->
+        <div class="box">
+            <div class="slider">
+            
+            </div>
+        </div>
+        <br>
+        <!--Section_1-->
+        <div class="box">
+            <div class="details-box1">
+                <h2 class="box-head">About</h2>
+
+                  At Interstellar, we believe that you would have better experience than you might have had watching the
+                  movie Interstellar. We aim at providing the advanced knowledge that might NEVER cost you to think all over
+                  again before proceeding any further. We won't be convincing you saying, "We are the best and you won't
+                  regret at any point." but we, at Interstellar, would put efforts from our head-to-toe to earn your trust
+                  and serve you the best.
+
+
+            </div> 
+
+            <div class="details-box1">
+                <h2 class="box-head">Company</h2>
+
+                  So this is our story and congratulations you will be walking with us from scratch to the top. Today, we
+                  are privileged to connect to you and many more young minds like you who actually know the real value and
+                  cost of time and money. we always believe in serving the best and that is why for encouraging young minds
+                  we have a team of these young minds.
+                  Our mission is to educate and make you enthusiastic in whatever you seek.
+
+
+            </div>
+
+            <div class="details-box1">
+                <h2 class="box-head">Services</h2>
+
+                <ul id="box3-list">
+                     <li> Our main aim is to serve you quality content which might exceed the quantity limits.</li>
+                     <li> Our aim is to make you shine bright holding an unique identity.</li>
+                     <li> Our aim is to make you feel delightful and make you say, "Woahh !! That's what i always wanted ! ".</li>
+                    </ul>
+            </div>        
+        </div>
+        <br>
+        <!--Section_2-->
+        <div class="box">
+            <div class="box1" id="detail">
+                <h1>Our Upcoming Schedule</h1>
+                <h2>Web Development</h2>
+                <h3>Topics Covered</h3>
+                <p>
+                   Exclusive - you have to paid 51 rs. with registeration.You will get certificate from mentor and us ,
+                   Udemy courses (free and paid with certificates at free ) and coursera courses kit and software kits and
+                   e-books. And you are join direct to the Google meet with guest for full time in webinar (7 days) . Daily
+                   you can clear your doubts with mentor and 24*7 doubt resolving program and support .
+                </p>
+           </div>
+            <div class="box1" id="links">
+                <h2 class="box-head">Follow us at</h2>
+                <ul class="social">
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCL4S-gy-Re2cv_-HJ9bg5Yw">Youtube</a>
+                    </li>
+                    <li>
+                        <a href="https://m.facebook.com/Computergurujee/">Facebook</a>
+                    </li>
+                    <li>
+                        <a href=" https://instagram.com/computerguruji008?igshid=gvmc9x60ubwh">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="https://forms.gle/LSdNvgFwBBajMwMBA">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <br>
+        <!--Slider_2-->
+        <div class="box">
+               <h2 id="img-slider">Gallery</h2>
+               <!--image sliding DC -->
+                <div class="slideshow-container-2">
+                    <div class="mySlides fade">
+                        <img src="./images/g1.png" style="width:100%; height:600px; padding: 10px 0;" alt="poster1">
+                    </div>
+                    <div class="mySlides fade">
+                        <img src="./images/g2.png" style="width:100%; height:600px; padding: 10px 0;" alt="poster2">
+                    </div>
+                    <div class="mySlides fade">
+                        <img src="./images/g3.png" style="width:60%; height:600px; padding: 10px 210px;" alt="poster3">
+                    </div>
+                    <div class="mySlides fade">
+                      <img src="./images/g4.png" style="width:60%; height:600px; padding: 10px 210px;" alt="poster4">
+                    </div>
+                </div>
+            </div>
+        <br>
+        <div style="text-align:center">
+          <span class="dot" onclick="currentSlide(1)"></span> 
+          <span class="dot" onclick="currentSlide(2)"></span> 
+          <span class="dot" onclick="currentSlide(3)"></span> 
+          <span class="dot" onclick="currentSlide(4)"></span> 
+        </div>
+        <br><br>
+        <!--footer-->
+        <footer>
+           <div class="box">
+                   <div class="foot" id="left">
+                          <p>&copy; COPYRIGHTS RESERVED BY INTERSTELLAR AND TEAM.</p>
+                      </div>
+                   <div class="foot" id="right">
+                        <a href="https://forms.gle/FmELFTCm9DjiYASFA" title="Developer Contact">info</a>
+                      </div>
+               </div>
+        </footer>
+        
+        <script>
+            var slideIndex = 1;
+            showSlides(slideIndex);
+
+            function plusSlides(n) {
+              showSlides(slideIndex += n);
+            }
+
+            function currentSlide(n) {
+              showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+              var i;
+              var slides = document.getElementsByClassName("mySlides");
+              var dots = document.getElementsByClassName("dot");
+              if (n > slides.length) {slideIndex = 1;}  
+              if (n < 1) {slideIndex = slides.length;}
+              for (i = 0; i < slides.length; i++) {
+                  slides[i].style.display = "none";  
+              }
+              for (i = 0; i < dots.length; i++) {
+                  dots[i].className = dots[i].className.replace(" active", "");
+              }
+              slides[slideIndex-1].style.display = "block";  
+              dots[slideIndex-1].className += " active";
+            }
+        </script>
+    </body>
+</html>
